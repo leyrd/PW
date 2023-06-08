9b1d098fb4e7c3f11952ac1ad0da33be2820db3c
leyrd <107440195+leyrd@users.noreply.github.com>
2023-06-07 22:52:27 -0500

    Delete mvc-invoice-2023-1-master directory

--- mvc-invoice-2023-1-master/src/main/java/com/uts/invoice/programacion/web/models/dao/IClienteDao.java
+++
@@ -1,9 +0,0 @@
-package com.uts.invoice.programacion.web.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.uts.invoice.programacion.web.models.entities.Cliente;
-
-public interface IClienteDao extends CrudRepository<Cliente,Long> {
-
-}
